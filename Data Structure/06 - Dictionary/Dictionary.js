d496508f5ec39ac6ff788e378ee29baad03a438f
Abdulrahman Alassaf. <s445013920@st.uqu.edu.sa>
2025-08-27 10:27:22 +0300

    Enhance Dictionary class with additional methods
    Added get, remove, size, and print methods to Dictionary class.

--- Data Structure/06 - Dictionary/Dictionary.js
+++ Data Structure/06 - Dictionary/Dictionary.js
@@ -1,4 +1,4 @@
-class Dictionary {
+module.exports = class Dictionary {
     constructor() {
         this.entries = [];
     }
@@ -12,6 +12,44 @@ class Dictionary {
         }
         let newPair = new Dictionary.KeyValuePair(key, value);
         this.entries.push(newPair);
+    }
+
+    get(key) {
+        for (let i = 0; i < this.entries.length; i++) {
+            if (this.entries[i] != null && this.entries[i].key == key) {
+                return this.entries[i].value;
+            }
+        }
+
+        return null;
+    }
+
+    remove(key) {
+        for (let i = 0; i < this.entries.length; i++) {
+            if (this.entries[i] != null && this.entries[i].key == key) {
+                let lastNode = this.entries.pop();
+                if (lastNode.key != key)
+                    this.entries[i] = lastNode;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    size() {
+        return this.entries.length;
+    }
+
+    print() {
+        console.log('-------------');
+        console.log('size: ' + this.size());
+
+        for (let i = 0; i < this.entries.length; i++) {
+            console.log('{' + this.entries[i].key + ': ' + this.entries[i].value + '}');
+        }
+        console.log('-------------');
     }
 
     // we need to use static key word to do nested classes or we put the class outside this class
