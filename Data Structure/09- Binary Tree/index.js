be358120587a20d0bb0280fa76b383ee4b607927
Abdulrahman Alassaf. <s445013920@st.uqu.edu.sa>
2025-08-28 22:11:11 +0300

    Update binary tree operations and add traversal methods

--- Data Structure/09- Binary Tree/index.js
+++ Data Structure/09- Binary Tree/index.js
@@ -2,12 +2,26 @@ let BinaryTree = require('./BinaryTree.js');
 
 let tree = new BinaryTree();
 
-tree.insert(1);
-tree.insert(2);
-tree.insert(3);
-tree.insert(4);
-tree.insert(5);
-tree.insert(6);
-tree.insert(7);
+tree.insert('A');
+tree.insert('B');
+tree.insert('C');
+tree.insert('D');
+tree.insert('E');
+tree.insert('F');
+tree.insert('G');
+tree.insert('H');
+tree.insert('I');
+
+tree.print();
+
+console.log('\nHeight: ' + tree.height());
+
+tree.preOrder();
+tree.inOrder();
+tree.postOrder();
+
+console.log(tree.find('A'));
+
+tree.delete('E');
 
 tree.print();
