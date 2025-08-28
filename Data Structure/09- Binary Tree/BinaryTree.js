f9127c400d7059c61262a2853130b6d402a38a94
Abdulrahman Alassaf. <s445013920@st.uqu.edu.sa>
2025-08-28 22:10:52 +0300

    Add find and delete methods to BinaryTree

--- Data Structure/09- Binary Tree/BinaryTree.js
+++ Data Structure/09- Binary Tree/BinaryTree.js
@@ -38,6 +38,98 @@ module.exports = class BinaryTree {
         return 1 + Math.max(this.height(node.left), this.height(node.right));
     }
 
+    find(data, node = this.root) {
+        if (node == null) return;
+        if (node.data == data) return node;
+
+        let leftResult = this.find(data, node.left);
+        if (leftResult != null) return leftResult;
+
+        return this.find(data, node.right);
+    }
+
+    delete(data) {
+        if (this.root == null) return;
+
+        if (this.root.left == null && this.root.right == null) {
+            if (this.root.data == data)
+                this.root = null;
+            return;
+        }
+
+        let queue = new Queue();
+        queue.enqueue(this.root);
+
+        let node, nodeToDelete, lastNode, parentOfLast;
+
+        while (queue.hasData()) {
+            node = queue.dequeue();
+
+            if (node.data == data)
+                nodeToDelete = node;
+
+            if (node.left != null) {
+                parentOfLast = node;
+                queue.enqueue(node.left);
+            }
+
+            if (node.right != null) {
+                parentOfLast = node;
+                queue.enqueue(node.right);
+            }
+
+            lastNode = node;
+        }
+
+        if (nodeToDelete) {
+            nodeToDelete.data = lastNode.data;
+
+            if (parentOfLast.right == lastNode)
+                parentOfLast.right = null;
+            else
+                parentOfLast.left = null;
+        }
+    }
+
+    preOrder(node = this.root) {
+        let result = [];
+        this.#internalPreOrder(node, result);
+        console.log(result.join(' -> '));
+    }
+
+    #internalPreOrder(node, result) {
+        if (node == null) return;
+        result.push(node.data);
+        this.#internalPreOrder(node.left, result);
+        this.#internalPreOrder(node.right, result);
+    }
+
+    inOrder(node = this.root) {
+        let result = [];
+        this.#internalInOrder(node, result);
+        console.log(result.join(' -> '));
+    }
+
+    #internalInOrder(node, result) {
+        if (node == null) return;
+        this.#internalInOrder(node.left, result);
+        result.push(node.data);
+        this.#internalInOrder(node.right, result);
+    }
+
+    postOrder(node = this.root) {
+        let result = [];
+        this.#internalPostOrder(node, result);
+        console.log(result.join(' -> '));
+    }
+
+    #internalPostOrder(node, result) {
+        if (node == null) return;
+        this.#internalPostOrder(node.left, result);
+        this.#internalPostOrder(node.right, result);
+        result.push(node.data);
+    }
+
     print() {
         if (this.root == null) {
             console.log("Tree is empty");
@@ -76,5 +168,4 @@ module.exports = class BinaryTree {
             this.right = null;
         }
     }
-
 }
