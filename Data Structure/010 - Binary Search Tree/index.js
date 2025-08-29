6afaca05d2be497fe1b2e4a8426829f8512c4abc
Abdulrahman Alassaf. <s445013920@st.uqu.edu.sa>
2025-08-29 22:15:46 +0300

    Refactor tree insertions and add balancing

--- Data Structure/010 - Binary Search Tree/index.js
+++ Data Structure/010 - Binary Search Tree/index.js
@@ -2,17 +2,17 @@ let BinarySearchTree = require('./BinarySearchTree.js');
 
 let tree = new BinarySearchTree();
 
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
 tree.insert(4);
+tree.insert(5);
 tree.insert(6);
 tree.insert(7);
-tree.insert(5);
-tree.insert(2);
-tree.insert(1);
-tree.insert(3);
 tree.print();
 
-console.log('');
+tree.balance();
 
-tree.delete(4);
+console.log('');
 
 tree.print();
